c8adff6ccb8b24b84519d69c8bce5a0e000eeb0a
Ankit Das <58646861+ankit0812-hue@users.noreply.github.com>
2020-04-22 11:01:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) =>{
+      console.log('Connected correctly to server');
+},(err) =>{
+      console.log(err);
+});
+const hostname = 'localhost';
+const port = 3000;
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+function auth(req,res,next){
+      var authHeader = req.headers.authorization;
+      if(!authHeader){
+         var err = new Error('You are not authenticated!');
+         res.setHeader('WWW-Authenticate','Basic');
+         err.status = 401;
+         next(err);
+      }
+      var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
+      if(auth[0]==='admin'&&auth[1]==='password'){
+            next();
+      }
+      else{
+            var err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate','Basic');
+            err.status = 401;
+            next(err);
+      }
+}
+app.use(auth);
+app.use(express.static(__dirname+ '/public'));
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
+app.use((req,res,next) => {
+      console.log(req.headers);
+      res.statusCode = 200;
+      res.setHeader('Content-Type','text/html');
+      res.end('<html><body><h1>Hello World</h1></body></html>');
+});
+const server = http.createServer(app);
+server.listen(port,hostname);
